d8a64400fc72f7522df2674363f7299567faf1dc
Lalitus <tallitosan@gmail.com>
2021-01-04 01:36:20 -0600

    Adding a UserList, UserItem Page/Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import UsersList from '../components/UsersList'
 
 const Users = () => {
-  return (
-    <h2>
-      User works!
-    </h2>
-  )
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Lalo Aguilar',
+      image:
+        'https://images.pexels.com/photos/939962/pexels-photo-939962.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      places: 5,
+    },
+  ]
+
+  return <UsersList items={USERS} />
 }
 
 export default Users
