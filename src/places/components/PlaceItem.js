40e1f719bb57579666491f6322893e05844e97d9
Lalitus <tallitosan@gmail.com>
2021-01-08 13:32:11 -0600

    implemented AuthContext in navlins

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
 import Modal from '../../shared/components/UIElements/Modal'
 import Map from '../../shared/components/UIElements/Map'
+import { AuthContext } from '../../shared/context/auth-context'
 import './PlaceItem.css'
 
 const PlaceItem = (props) => {
+  const auth = useContext(AuthContext)
   const [showMap, setshowMap] = useState(false)
   const [showConfirmModal, setShowConfirmModal] = useState(false)
 
@@ -68,10 +70,14 @@ const PlaceItem = (props) => {
             <Button inverse onClick={openMapHandler}>
               View on Map
             </Button>
-            <Button to={`/places/${props.id}`}>Edit</Button>
-            <Button danger onClick={showDeleteWarningHandler}>
-              Delete
-            </Button>
+            {auth.isLoggedIn && (
+              <Button to={`/places/${props.id}`}>Edit</Button>
+            )}
+            {auth.isLoggedIn && (
+              <Button danger onClick={showDeleteWarningHandler}>
+                Delete
+              </Button>
+            )}
           </div>
         </Card>
       </li>
